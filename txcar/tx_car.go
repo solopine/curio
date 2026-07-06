583094bb921ff54cb116fbc8e4c4a86f573dfa3e
solopine <solopine@gmail.com>
2026-07-06 21:20:53 +0800

    init1

--- txcar/tx_car.go
+++ txcar/tx_car.go
@@ -3,8 +3,6 @@ package txcarlib
 import (
 	"os"
 
-	addr "github.com/filecoin-project/go-address"
-	lpiece "github.com/filecoin-project/lotus/storage/pipeline/piece"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/solopine/txcar/txcar"
 )
@@ -16,26 +14,6 @@ const TxHttpPort = 24001
 func ParseTxPieceFromUrlPath(path string) (*txcar.TxPiece, error) {
 	p, err := txcar.BoostPathParser.Parse(path)
 	return &p, err
-}
-
-func ParseTxPieceFromDeal(deal lpiece.PieceDealInfo) (*txcar.TxPiece, error) {
-
-	if deal.PieceActivationManifest == nil {
-		return nil, nil
-	}
-
-	if len(deal.PieceActivationManifest.Notify) != 1 {
-		return nil, nil
-	}
-	notify := deal.PieceActivationManifest.Notify[0]
-	if notify.Address != addr.Undef {
-		return nil, nil
-	}
-	if notify.Payload == nil {
-		return nil, nil
-	}
-	txCarInfoStr := string(notify.Payload)
-	return ParseTxPiece(txCarInfoStr)
 }
 
 func CreateFakeUnsealedFile(filePath string, txPiece txcar.TxPiece) error {
